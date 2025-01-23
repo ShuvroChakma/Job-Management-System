fffa3ccec1038ff36a43d22d99dcde01c2ae89f9
ShuvroChakma <shubrachakma101@gmail.com>
2025-01-24 01:24:59 +0600

    Front end complete

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,8 @@ import { jobsData } from "../assets/assets";
 
     const[jobs, setJobs] = useState([])
 
+    const [showRecruiterLogin, setShowRecruiterLogin] = useState(false)
+
     // Function to fetch jobs
     const fetchJobs = async () =>{
         setJobs(jobsData)
@@ -26,7 +28,8 @@ import { jobsData } from "../assets/assets";
     const value = {
         setSearchFilter, searchFilter,
         isSearched, setIsSearched,
-        jobs, setJobs
+        jobs, setJobs,
+        showRecruiterLogin, setShowRecruiterLogin,
 
     }
 
