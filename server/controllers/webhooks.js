8ce87c00596d5670f10f879f0f46fa4876a6a323
ShuvroChakma <shubrachakma101@gmail.com>
2025-01-26 20:12:10 +0600

    update

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -6,15 +6,15 @@ import User from "../models/User.js";
 export const clerkWebhooks = async(req, res)=>{
     try {
         // create svix instance with clerk webhook secret
-        const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET)
+        // const whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET)
 
 
         // //  verify headers
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id" : req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        })
+        // await whook.verify(JSON.stringify(req.body),{
+        //     "svix-id" : req.headers["svix-id"],
+        //     "svix-timestamp": req.headers["svix-timestamp"],
+        //     "svix-signature": req.headers["svix-signature"]
+        // })
  
         // getting data from request body
      
