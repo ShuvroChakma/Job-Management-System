e3a94b97a3db405846fe76285dfa47c9b39024d3
ShuvroChakma <shubrachakma101@gmail.com>
2025-01-26 18:59:44 +0600

    error message

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -17,7 +17,7 @@ export const clerkWebhooks = async(req, res)=>{
         })
 
         // getting data from request body
-        dgeg
+     
         const {data,type} = req.body
         
 
@@ -69,6 +69,6 @@ export const clerkWebhooks = async(req, res)=>{
 
     } catch (error) {
         console.log("error.message");
-        res.json({success:false, message: 'Webhooks Error!'})
+        res.json({success:false, message: 'Webhooks Error! '+error.message()})
     }
 }
