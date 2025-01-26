40859968787f11f581981b783aa826ea77306fe7
ShuvroChakma <shubrachakma101@gmail.com>
2025-01-26 20:04:58 +0600

    update

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ app.use(express.json())
 // Routes
 
 app.get('/',async(req,res)=> {
-
-;})
+ res.send({})
+})
 app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
