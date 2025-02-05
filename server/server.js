ca351323d294e67ab92cca37996d97cc3e7e9a79
ShuvroChakma <shubrachakma101@gmail.com>
2025-02-05 15:43:59 +0600

    solve issue

--- server/server.js
+++ server/server.js
@@ -1,51 +1,49 @@
-import './configs/instrument.js'
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/db.js'
+import "./configs/instrument.js";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
 import * as Sentry from "@sentry/node";
-import { clerkWebhooks } from './controllers/webhooks.js'
-import User from './models/User.js'
-import companyRoutes from './routes/companyRoutes.js'
-import connectCloudinary from './configs/cloudinary.js'
-import jobRoutes from './routes/jobRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import {clerkMiddleware} from'@clerk/express'
+import { clerkWebhooks } from "./controllers/webhooks.js";
+import User from "./models/User.js";
+import companyRoutes from "./routes/companyRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import jobRoutes from "./routes/jobRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 // Initialize Express
 
-const app = express()
+const app = express();
 
 // connect to database
-await connectDB()
-await connectCloudinary()
+await connectDB();
+await connectCloudinary();
 
 // Middlewares
 
-app.use(cors())
-app.use(express.json())
-app.use(clerkMiddleware())
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 // Routes
 
-app.get('/',async(req,res)=> {
- res.send({})
-})
+app.get("/", async (req, res) => {
+  res.send("Api working!");
+});
 app.get("/debug-sentry", function mainHandler(req, res) {
-    throw new Error("My first Sentry error!");
-  });
-app.post('/webhooks', clerkWebhooks)
-app.use('/api/company', companyRoutes)
-app.use('/api/jobs', jobRoutes)
-app.use('/api/users', userRoutes)
-  
+  throw new Error("My first Sentry error!");
+});
+app.post("/webhooks", clerkWebhooks);
+app.use("/api/company", companyRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/users", userRoutes);
 
 // Port
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 Sentry.setupExpressErrorHandler(app);
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
